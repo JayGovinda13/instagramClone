3297a81941af244fbb38aba13b533d9a093f8252
Juliano Amaral <jaygovinda13@gmail.com>
2024-04-12 13:07:44 -0300

    Design de sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import AuthPage from "./pages/AuthPage/AuthPage";
+import PageLayouts from "./Layouts/PageLayout/PageLayouts";
 
 function App() {
   return (
-    <>
+    <PageLayouts>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/auth" element={<AuthPage />} />
       </Routes>
-    </>
+    </PageLayouts>
   );
 }
 
